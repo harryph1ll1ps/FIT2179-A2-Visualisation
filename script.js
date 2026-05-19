2fc288cd29fb7f63ae8771612edd2e011dfd7b02
harryph1ll1ps <harry.t.phillips1@gmail.com>
2026-05-19 16:16:47 +1000

    Bust cached Pages assets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-vegaEmbed("#vis1", "vis/vis1.json", { actions: false });
-vegaEmbed("#vis2", "vis/vis2.json", { actions: false });
-vegaEmbed("#vis3", "vis/vis3.json", { actions: false });
-vegaEmbed("#vis4", "vis/vis4.json", { actions: false });
-vegaEmbed("#map", "vis/map.json", { actions: false });
+vegaEmbed("#vis1", "vis/vis1.json?v=20260519", { actions: false });
+vegaEmbed("#vis2", "vis/vis2.json?v=20260519", { actions: false });
+vegaEmbed("#vis3", "vis/vis3.json?v=20260519", { actions: false });
+vegaEmbed("#vis4", "vis/vis4.json?v=20260519", { actions: false });
+vegaEmbed("#map", "vis/map.json?v=20260519", { actions: false });
