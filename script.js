890a416ee054dc512b90f6b0533488a1192101c6
harryph1ll1ps <harry.t.phillips1@gmail.com>
2026-05-27 21:38:41 +1000

    choropleth map

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ vegaEmbed("#vis2", "vis/vis2.json", { actions: false });
 vegaEmbed("#vis3", "vis/vis3.json", { actions: false });
 vegaEmbed("#vis4", "vis/vis4.json", { actions: false });
 vegaEmbed("#vis5", "vis/vis5.json", { actions: false });
-vegaEmbed("#vis6", "vis/vis6.json", { actions: false });
+vegaEmbed("#vis6",     "vis/vis6.json",     { actions: false });
+vegaEmbed("#vis-choro", "vis/vis_choro.json", { actions: false });
 vegaEmbed("#vis7", "vis/vis7.json", { actions: false });
