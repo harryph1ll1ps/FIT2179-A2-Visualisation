e7983ca741c7df62bbec736d72a0e06187de8785
harryph1ll1ps <harry.t.phillips1@gmail.com>
2026-05-27 20:35:06 +1000

    change to nuclear datasets & create map 1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-vegaEmbed("#vis1", "vis/vis1.json?v=20260519", { actions: false });
-vegaEmbed("#vis2", "vis/vis2.json?v=20260519", { actions: false });
-vegaEmbed("#vis3", "vis/vis3.json?v=20260519", { actions: false });
-vegaEmbed("#vis4", "vis/vis4.json?v=20260519", { actions: false });
-vegaEmbed("#map", "vis/map.json?v=20260519", { actions: false });
+vegaEmbed("#vis1", "vis/vis1.json", { actions: false });
+vegaEmbed("#vis2", "vis/vis2.json", { actions: false });
+vegaEmbed("#vis3", "vis/vis3.json", { actions: false });
+vegaEmbed("#vis4", "vis/vis4.json", { actions: false });
+vegaEmbed("#vis5", "vis/vis5.json", { actions: false });
+vegaEmbed("#vis6", "vis/vis6.json", { actions: false });
+vegaEmbed("#vis7", "vis/vis7.json", { actions: false });
